8efae2bda3d9b6f747d4aef890b8b01789e35f5d
Stanisław Olejnik <ztach@poczta.onet.pl>
2018-06-12 12:48:12 +0200

    koniec yesNo

---
+++ WFAapp1/komunikaty/YesNo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WFAapp1.komunikaty
+{
+    public partial class YesNo : UserControl
+    {
+        public YesNo()
+        {
+            InitializeComponent();
+        }
+        public bool yesno;
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            yesno = true;
+        }
+
+        private void bbtnNo_Click(object sender, EventArgs e)
+        {
+            yesno = false;
+        }
+    }
+}
